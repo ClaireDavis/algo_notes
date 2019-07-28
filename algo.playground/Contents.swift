e261092ac5e5ec18f6bad101ce14b693c0a00506
Rebecca Kukshtel <rebecca@betterment.com>
2019-07-28 12:50:59 -0400

    extract insert

--- algo.playground/Contents.swift
+++ algo.playground/Contents.swift
@@ -1,42 +1,18 @@
-import UIKit
 import Foundation
 
 let tree = Node(data: 2)
-
-// INSERTION
-func insert(node: Node, into tree: Node) {
-    guard node.data != tree.data else {
-        fatalError("I can't handle duplicates!")
-    }
-
-    if node.data < tree.data {
-        if let left = tree.left {
-            insert(node: node, into: left)
-        } else {
-            tree.left = node
-            node.parent = tree
-        }
-    } else {
-        if let right = tree.right {
-            insert(node: node, into: right)
-        } else {
-            tree.right = node
-            node.parent = tree
-        }
-    }
-}
-
 
 let nine = Node(data: 9)
 let twelve = Node(data: 12)
 let ten = Node(data: 10)
 
+// INSERT
 insert(node: nine, into: tree)
 insert(node: Node(data: 1), into: tree)
 insert(node: twelve, into: tree)
 insert(node: ten, into: tree)
 
+// CHECK INSERT
 print(format(tree))
 print(format(nine))
 print(format(twelve))
-
--- algo.playground/Sources/Insert.swift
+++ algo.playground/Sources/Insert.swift
@@ -1,2 +1,24 @@
 import Foundation
 
+// INSERTION
+public func insert(node: Node, into tree: Node) {
+    guard node.data != tree.data else {
+        fatalError("I can't handle duplicates!")
+    }
+
+    if node.data < tree.data {
+        if let left = tree.left {
+            insert(node: node, into: left)
+        } else {
+            tree.left = node
+            node.parent = tree
+        }
+    } else {
+        if let right = tree.right {
+            insert(node: node, into: right)
+        } else {
+            tree.right = node
+            node.parent = tree
+        }
+    }
+}
