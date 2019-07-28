b315e44df3348fac01a2657e275e8e57fca98ce2
Rebecca Kukshtel <rebecca@betterment.com>
2019-07-28 12:45:45 -0400

    extract node to source

--- algo.playground/Contents.swift
+++ algo.playground/Contents.swift
@@ -1,58 +1,6 @@
 import UIKit
 import Foundation
 
-// TODO: Make generic >_>
-class Node {
-    let data: Int
-    var parent: Node?
-    var left: Node?
-    var right: Node?
-
-    init(data: Int, parent: Node? = nil, left: Node? = nil, right: Node? = nil) {
-        self.data = data
-        self.parent = parent
-        self.left = left
-        self.right = right
-    }
-
-    private var parentString: String {
-        if let p = parent {
-            return "parent: \(p.data)"
-        } else {
-            return "parent: none"
-        }
-    }
-
-    private var leftString: String {
-        if let l = left {
-            return "left: \(l.data)"
-        } else {
-            return "left: none"
-        }
-    }
-
-    private var rightString: String {
-        if let r = right {
-            return "right: \(r.data)"
-        } else {
-            return "right: none"
-        }
-    }
-
-    var description: String {
-        return """
-        data: \(data)
-        \(parentString)
-        \(leftString)
-        \(rightString)
-"""
-    }
-}
-
-func format(_ node: Node) -> String {
-    return node.description
-}
-
 let tree = Node(data: 2)
 
 // INSERTION
@@ -89,9 +37,6 @@ insert(node: twelve, into: tree)
 insert(node: ten, into: tree)
 
 print(format(tree))
-print()
 print(format(nine))
-print()
 print(format(twelve))
-
 
--- algo.playground/Sources/Node.swift
+++ algo.playground/Sources/Node.swift
@@ -1,2 +1,59 @@
+
 import Foundation
 
+// TODO: Make generic >_>
+public class Node {
+    public let data: Int
+    public var parent: Node?
+    public var left: Node?
+    public var right: Node?
+
+    public init(
+        data: Int,
+        parent: Node? = nil,
+        left: Node? = nil,
+        right: Node? = nil) {
+        self.data = data
+        self.parent = parent
+        self.left = left
+        self.right = right
+    }
+
+    private var parentString: String {
+        if let p = parent {
+            return "parent: \(p.data)"
+        } else {
+            return "parent: none"
+        }
+    }
+
+    private var leftString: String {
+        if let l = left {
+            return "left: \(l.data)"
+        } else {
+            return "left: none"
+        }
+    }
+
+    private var rightString: String {
+        if let r = right {
+            return "right: \(r.data)"
+        } else {
+            return "right: none"
+        }
+    }
+
+    public var description: String {
+        return """
+        \n
+        data: \(data)
+        \(parentString)
+        \(leftString)
+        \(rightString)
+        """
+    }
+}
+
+public func format(_ node: Node) -> String {
+    return node.description
+}
